c2ee57859501bc38213a186d3da273cb95d29adf
clowwindy <clowwindy42@gmail.com>
2014-07-15 14:24:12 +0800

    Update stackscript.sh

--- stackscript.sh
+++ stackscript.sh
@@ -8,7 +8,7 @@ PORTS_USED=`netstat -antl |grep LISTEN | awk '{ print $4 }' | cut -d: -f2|sed '/
 PORTS_USED=`echo $PORTS_USED|sed 's/\s/$\|^/g'`
 PORTS_USED="^${PORTS_USED}$"
 
-SS_PASSWORD=`dd if=/dev/random bs=32 count=1 | md5sum | cut -c-32`
+SS_PASSWORD=`dd if=/dev/urandom bs=32 count=1 | md5sum | cut -c-32`
 SS_PORT=`seq 1025 9000 | grep -v -E "$PORTS_USED" | shuf -n 1`
 
 wget https://raw.githubusercontent.com/shadowsocks/stackscript/master/shadowsocks.json -O /etc/shadowsocks.json
